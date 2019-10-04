636c0b371fe58ca9ed42b42612df940ebacf04cb
verapaneni <verapaneni.nalini@gmail.com>
2019-10-04 16:11:16 +0530

    commited code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
                                         Toolbar toolbar = findViewById(R.id.toolbar);
                                         setSupportActionBar(toolbar);
 
+                                        int d=10;
                                         FloatingActionButton fab = findViewById(R.id.fab);
                                         fab.setOnClickListener(new View.OnClickListener() {
                                                             @Override
